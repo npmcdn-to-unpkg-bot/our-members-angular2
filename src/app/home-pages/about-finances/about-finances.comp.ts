594daefa39aa99ab469f637df6bacfc2b3e45360
helix46 <bryce.abbott@y7mail.com>
2016-04-26 22:20:27 +1000

    finances

--- src/app/home-pages/about-finances/about-finances.comp.ts
+++ src/app/home-pages/about-finances/about-finances.comp.ts
@@ -5,7 +5,7 @@ import {RouteConfig, ROUTER_DIRECTIVES} from 'angular2/router';
     selector: 'about-finances',
 
     templateUrl: 'src/app/home-pages/about-finances/about-finances.html',
-    styleUrls: ['src/app/home-pages/styles/home-pages.css'],
+    styleUrls: ['src/app/home-pages/styles/home-pages.css', 'src/app/home-pages/about-finances/about-finances.css'],
     directives: [ROUTER_DIRECTIVES]
 })
 
