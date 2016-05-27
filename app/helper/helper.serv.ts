abde89242cc2a481182ab855ad43332af834b51e
helix46 <bryce.abbott@y7mail.com>
2016-05-27 14:50:23 +1000

    change url

--- app/helper/helper.serv.ts
+++ app/helper/helper.serv.ts
@@ -29,9 +29,9 @@ export class HelperService {
 
     static getServiceBase(): string {
         //return 'https://??????????.azurewebsites.net/';
-        //return 'https://ourmembersapiweb.azurewebsites.net/'
+        return 'https://ourmembersapiweb.azurewebsites.net/'
         //return 'https://microsoft-apiappc5976b26a6ac40f2988bf2e315f7d032.azurewebsites.net/'
-        return 'http://localhost:4271/';
+        //return 'http://localhost:4271/';
     }
 
     static getTokenName(): string {
