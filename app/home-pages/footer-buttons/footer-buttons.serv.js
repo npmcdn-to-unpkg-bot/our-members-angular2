41f22206e8c0620aa4f160e00cea3cefa2441797
helix46 <bryce.abbott@y7mail.com>
2016-08-18 08:28:08 +1000

    rc5 footer buttons

---
+++ app/home-pages/footer-buttons/footer-buttons.serv.js
@@ -0,0 +1,33 @@
+"use strict";
+var __decorate = (this && this.__decorate) || function (decorators, target, key, desc) {
+    var c = arguments.length, r = c < 3 ? target : desc === null ? desc = Object.getOwnPropertyDescriptor(target, key) : desc, d;
+    if (typeof Reflect === "object" && typeof Reflect.decorate === "function") r = Reflect.decorate(decorators, target, key, desc);
+    else for (var i = decorators.length - 1; i >= 0; i--) if (d = decorators[i]) r = (c < 3 ? d(r) : c > 3 ? d(target, key, r) : d(target, key)) || r;
+    return c > 3 && r && Object.defineProperty(target, key, r), r;
+};
+var __metadata = (this && this.__metadata) || function (k, v) {
+    if (typeof Reflect === "object" && typeof Reflect.metadata === "function") return Reflect.metadata(k, v);
+};
+var router_1 = require("@angular/router");
+var core_1 = require("@angular/core");
+var http_handler_serv_1 = require("../../services/http-handler/http-handler.serv");
+var http_1 = require("@angular/http");
+var FooterButtonsService = (function () {
+    function FooterButtonsService(http, router) {
+        this.http = http;
+        this.router = router;
+        console.log('constructor CountriesService');
+    }
+    FooterButtonsService.prototype.getDevOrProd = function () {
+        var parameters = [];
+        var httpHandlerService = new http_handler_serv_1.HttpHandlerService(this.http, this.router);
+        return httpHandlerService.getObject(parameters, 'api/organisations/get-dev-or-prod', false);
+    };
+    FooterButtonsService = __decorate([
+        core_1.Injectable(), 
+        __metadata('design:paramtypes', [http_1.Http, router_1.Router])
+    ], FooterButtonsService);
+    return FooterButtonsService;
+}());
+exports.FooterButtonsService = FooterButtonsService;
+//# sourceMappingURL=footer-buttons.serv.js.map
---
+++ app/home-pages/footer-buttons/footer-buttons.serv.ts
@@ -0,0 +1,18 @@
+﻿import {Router} from "@angular/router";
+import {Injectable} from "@angular/core";
+import {HttpHandlerService} from "../../services/http-handler/http-handler.serv";
+import {Http} from "@angular/http";
+
+@Injectable()
+export class FooterButtonsService {
+    constructor(private http: Http, private router: Router) {
+        console.log('constructor CountriesService');
+    }
+
+    getDevOrProd() {
+        var parameters: modSharedTypes.IHttpParameter[] = [];
+        var httpHandlerService = new HttpHandlerService(this.http, this.router);
+        return httpHandlerService.getObject(parameters, 'api/organisations/get-dev-or-prod', false);
+    }
+}
+
