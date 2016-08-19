1f8041d5a4d50be1f4016a8af6442087c603d974
helix46 <bryce.abbott@y7mail.com>
2016-08-20 06:36:19 +1000

    choose members 2

---
+++ app/organisation-admin-pages/email-member/email-member.comp.ts
@@ -0,0 +1,65 @@
+import {Component, OnInit} from "@angular/core";
+import {Router} from "@angular/router";
+import {HelperService} from "../../services/helper/helper.serv";
+import {AgGridNg2} from "ag-grid-ng2/main";
+import {EmailMemberService} from "../email-member/email-member.serv";
+import {ChooseMembersComponent} from "../choose-members/choose-members.comp";
+
+@Component({
+    moduleId: module.id,
+    selector: 'email-member',
+    templateUrl: 'email-member.html',
+    styleUrls: ['email-member.css'],
+    providers: [EmailMemberService],
+    directives: [AgGridNg2, ChooseMembersComponent]
+})
+
+
+export class EmailMemberComponent implements OnInit {
+    constructor(private router: Router, private emailMemberService: EmailMemberService) {
+        HelperService.log('constructor EmailComponent');
+    }
+
+    ngOnInit() {
+        //this.Member = this.getEmptyMember(0);
+    }
+
+    applyFiltersNext = ()=> {
+        this.hideAll();
+        //this.memberFilter = true;
+    }
+    // memberFilterBack = ()=> {
+    //     this.hideAll();
+    //     this.applyFilters = true;
+    // }
+    // memberFilterNext = ()=> {
+    //     this.hideAll();
+    //     this.subjectAndBody = true;
+    // }
+    // subjectAndBodyBack = ()=> {
+    //     this.hideAll();
+    //     this.memberFilter = true;
+    // }
+    // subjectAndBodySend = ()=> {
+    //     this.sendEmail();
+    // }
+    // subjectAndBodyGoToAttachments = ()=> {
+    //     this.hideAll();
+    //     this.attachments = true;
+    // }
+    // attachmentsBack = ()=> {
+    //     this.hideAll();
+    //     this.subjectAndBody = true;
+    // }
+
+    hideAll = ()=> {
+        this.chooseMembers = false;
+        // this.memberFilter = false;
+        // this.subjectAndBody = false;
+        // this.attachments = false;
+
+    }
+    sendEmail=()=>{
+    }
+    chooseMembers: boolean = true;
+}
